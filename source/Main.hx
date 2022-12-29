3ce6f81074f6d5c5e2a91d73367bb235d889d4a1
TheKitBoi <84044666+TheKitBoi@users.noreply.github.com>
2022-12-29 05:14:50 +0000

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -5,9 +5,12 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
+
+	var framerate:Int = 60;
+	
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState.new));
+		addChild(new FlxGame(1280, 720, PlayState, #if (flixel < "5.0.0") 1, #end framerate, framerate, true, false));
 	}
 }
