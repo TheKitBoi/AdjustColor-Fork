95c91533e2335e17391f9c5f2299ecc919ab2ecc
Ratio-Combinder <120575989+Ratio-Combinder@users.noreply.github.com>
2023-01-03 11:04:37 +0000

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -9,9 +9,15 @@ import flxanimate.motion.AdjustColor;
 class PlayState extends FlxState
 {
 	var adjust = new AdjustColor();
+	var adjust:AdjustColor = null;
 
 	override public function create()
 	{
+		adjust.brightness = 0;
+		adjust.contrast = 0;
+		adjust.saturation = 0;
+		adjust.hue = 0;
+		
 		var background = FlxGridOverlay.create(10, 10);
 		add(background);
 		super.create();
@@ -19,11 +25,6 @@ class PlayState extends FlxState
 
 	override public function update(elapsed:Float)
 	{
-		adjust.brightness = 0;
-		adjust.contrast = 0;
-		adjust.saturation = 0;
-		adjust.hue = 0;
-		
 		var bf = new FlxSprite(AssetPaths.bf__png);
 		bf.screenCenter();
 		bf.antialiasing = true;
@@ -37,21 +38,21 @@ class PlayState extends FlxState
 		add(filteredbf);
 		
 		if (FlxG.keys.pressed.W)
-			adjust.brightness -= elapsed * 0.1;
+			bf.shader.brightness -= elapsed * 0.1;
 		if (FlxG.keys.pressed.S)
-			adjust.brightness += elapsed * 0.1;
+			bf.shader.brightness += elapsed * 0.1;
 		if (FlxG.keys.pressed.R)
-			adjust.contrast -= elapsed * 0.1;
+			bf.adjust.contrast -= elapsed * 0.1;
 		if (FlxG.keys.pressed.F)
-			adjust.contrast += elapsed * 0.1;
+			bf.adjust.contrast += elapsed * 0.1;
 		if (FlxG.keys.pressed.Y)
-			adjust.saturation -= elapsed * 0.1;
+			bf.adjust.saturation -= elapsed * 0.1;
 		if (FlxG.keys.pressed.H)
-			adjust.saturation += elapsed * 0.1;
+			bf.adjust.saturation += elapsed * 0.1;
 		if (FlxG.keys.pressed.I)
-			adjust.hue -= elapsed * 0.1;
+			bf.adjust.hue -= elapsed * 0.1;
 		if (FlxG.keys.pressed.K)
-			adjust.hue += elapsed * 0.1;
+			bf.adjust.hue += elapsed * 0.1;
 		
 		super.update(elapsed);
 	}
