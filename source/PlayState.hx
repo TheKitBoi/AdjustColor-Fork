a191eaf859d2933dba3eb3c047d2d43cabd5360d
TheKitBoi <84044666+TheKitBoi@users.noreply.github.com>
2023-01-19 16:43:31 +0000

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -36,7 +36,7 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		if (FlxG.keys.pressed.R)
-			PlayState.resetState();
+			LoadingState.loadAndSwitchState(new PlayState());
 		
 		super.update(elapsed);
 	}
